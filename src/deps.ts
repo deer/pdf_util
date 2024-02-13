8d65d00f7a8c6d942a06d9d2db7ee69886fd10ea
Reed von Redwitz <reed+github@vonredwitz.com>
2024-02-13 13:14:47 +0100

    feat: add release workflow

--- src/deps.ts
+++ src/deps.ts
@@ -5,3 +5,9 @@ export {
   UpgradeCommand,
 } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 export { PDFDocument } from "https://esm.sh/@pdfme/pdf-lib@1.18.3";
+export {
+  format,
+  increment,
+  parse,
+  type ReleaseType,
+} from "https://deno.land/std@0.215.0/semver/mod.ts";
